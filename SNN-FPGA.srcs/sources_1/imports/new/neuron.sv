cb3b6336a5dea49b97f66e439fa4ace30ae0dfeb
Divyanshu Minz <dvnshmnz@gmail.com>
2022-04-26 21:39:13 +0530

    init

---
+++ SNN-FPGA.srcs/sources_1/imports/new/neuron.sv
@@ -0,0 +1,94 @@
+`timescale 1ns / 1ns
+
+module neuron # (
+    parameter
+    PREV_LAYER_NEURONS,
+    WEIGHT_WIDTH = 32,
+    POTENT_WIDTH = 48
+)(
+    input  logic  clk,
+    input  logic  rst,
+    input  logic  en,
+    
+    // weight memory write port
+    input  logic  [9:0] waddr,
+    input  logic  [WEIGHT_WIDTH-1 : 0] wdata,
+    input  logic  wen,
+    
+    input  logic  [PREV_LAYER_NEURONS-1 : 0] spike_in,
+    output logic  spike_out 
+);
+    
+    logic signed [POTENT_WIDTH-1 : 0] potent;  // membrane potential of this neuron
+    localparam   potent_thres = 'h000600000000;      // threshold potential to trigger a spike
+    localparam   potent_rest  = 'h000280000000;      // resting potential
+    
+    logic signed [WEIGHT_WIDTH-1 : 0] synapses_weight[PREV_LAYER_NEURONS-1 : 0];
+    logic signed [WEIGHT_WIDTH-1 : 0] potent_in[PREV_LAYER_NEURONS-1 : 0];  // won't exceed weight width
+    logic signed [POTENT_WIDTH-1 : 0] sum_potent_in;
+    
+    logic       refrac_en;
+    logic [3:0] refrac_timer;  // refractory period
+    localparam  refrac_len = 'd1;
+    
+    always_ff @(posedge clk)
+    begin
+        if (wen) synapses_weight[waddr] <= wdata;  // must manually initialize; don't reset
+    end
+    
+    always_comb
+    begin
+        for (integer i = 0; i < PREV_LAYER_NEURONS; i = i+1)
+            potent_in[i] = spike_in[i] ? synapses_weight[i] : '0;
+    end
+    
+    always_comb
+    begin
+        sum_potent_in = '0;
+        for (integer i = 0; i < PREV_LAYER_NEURONS; i = i+1)
+            sum_potent_in = sum_potent_in + potent_in[i];
+    end
+    
+    //handle potential assignment after refractory period/spike
+    always_ff @ (posedge clk)
+    begin
+        if (rst) begin
+            potent    <= potent_rest;
+            spike_out <= '0;
+        end
+        else if (en) begin
+            if (refrac_en == 1'b0) begin    //not in refrac period
+                potent <= potent + sum_potent_in;
+                if (potent >= potent_thres) begin
+                    spike_out <= 1'b1;  // trigger a spike
+                    potent    <= potent_rest;
+                end
+                if (spike_out == 1'b1) begin
+                    // potent    <= potent_rest;
+                    spike_out <= 1'b0;
+                end
+            end
+            else begin  // in refractory period
+                potent    <= potent;
+                spike_out <= 1'b0;
+            end
+        end 
+    end
+    
+    always_ff @ (posedge clk)
+    begin
+        if (rst) begin
+            refrac_en    <= '0;
+            refrac_timer <= '0;
+        end
+        else if (en) begin
+            if (spike_out == 1'b1) refrac_en <= 1'b1;  // enter refractory period in next clock
+            if (refrac_timer == refrac_len - 1) begin  // end of refractory period
+                refrac_en    <= 1'b0;
+                refrac_timer <= '0;
+            end
+            else if (refrac_en) refrac_timer <= refrac_timer + 1'b1;
+        end
+    end 
+    
+endmodule
---
+++ SNN-FPGA.srcs/sources_1/imports/new/snn.sv
@@ -0,0 +1,125 @@
+`timescale 1ns / 1ns
+
+module snn #(
+    parameter
+    WEIGHT_WIDTH = 32,
+    POTENT_WIDTH = 48,
+    TIMESTEP_MAX = 200
+)(
+    input  logic  clk,
+    input  logic  rst,
+    input  logic  en,
+    
+    output logic [3:0] result
+);
+
+    localparam INPUT_LAYER_NEURONS = 'd784;
+    localparam HIDDEN_LAYER_NEURONS = 'd100;  // currently only one hidden layer
+    localparam BRAM_LATENCY = 'd2;
+    localparam INIT_STEPS = INPUT_LAYER_NEURONS + BRAM_LATENCY;
+    localparam TIMESTEP_MAX_I = TIMESTEP_MAX + BRAM_LATENCY;  // num of lines in the input coe file + bram latency
+    localparam CNT_WIDTH = 'd7;  // width of spiking cnt, = width of TIMESTEP/2
+
+    logic [9:0] init_timer;  // [0, 784); iterate over the synaptic weight ROM to load weight into neurons
+    logic init_ren, init_wen, init_done;
+    always_ff @(posedge clk)
+    begin
+        if (rst) init_timer <= 1'b0;
+        else if (en && !init_done) init_timer <= init_timer + 1'b1;
+    end
+    
+    assign init_ren = init_timer < INPUT_LAYER_NEURONS + 1'd1;  // add 1 clock to ensure the last read is done (only necessary for the BRAM IP)
+    assign init_wen = init_timer >= BRAM_LATENCY && init_timer < INIT_STEPS;
+    assign init_done = init_timer == INIT_STEPS;
+    
+    logic [HIDDEN_LAYER_NEURONS * WEIGHT_WIDTH - 1 : 0] init_weight; 
+//    weight_mem u_weight_mem(
+//        .clka(clk),
+//        .ena(en && init_ren),
+//        .addra(init_timer),
+//        .douta(init_weight)
+//    );
+    
+
+    logic [7:0] timestep;  // [0, 200)
+    logic timestep_en;
+    always_ff @(posedge clk)
+    begin
+        if (rst) timestep <= '0;
+        else if (en && init_done && timestep_en) timestep <= timestep + 1'b1;
+    end
+    assign timestep_en = timestep < TIMESTEP_MAX_I;  // freeze after all simulation timestep elapsed
+
+    logic [INPUT_LAYER_NEURONS-1 : 0] input_spike;
+//    input_mem u_input_mem(
+//        .clka(clk),
+//        .ena(en && timestep < TIMESTEP_MAX + 'd1),
+//        .addra(timestep),
+//        .douta(input_spike)
+//    );
+    
+    
+    logic [HIDDEN_LAYER_NEURONS-1 : 0] hidden_layer_spike;
+    hidden_layer #(
+        .WEIGHT_WIDTH(WEIGHT_WIDTH),
+        .POTENT_WIDTH(POTENT_WIDTH),
+        .PREV_LAYER_NEURONS(INPUT_LAYER_NEURONS),
+        .CURR_LAYER_NEURONS(HIDDEN_LAYER_NEURONS)
+    ) u_hidden_layer(
+        .clk(clk),
+        .rst(rst),
+        .en(en && timestep_en),
+        
+        .waddr(init_timer - BRAM_LATENCY),
+        .wdata(init_weight),
+        .wen(en && init_wen),
+        
+        .prev_layer_spike(input_spike),
+        .curr_layer_spike(hidden_layer_spike)
+    );
+    
+    logic [CNT_WIDTH-1 : 0] hidden_layer_spike_cnt[HIDDEN_LAYER_NEURONS-1 : 0];
+    generate
+        for (genvar i = 0; i < HIDDEN_LAYER_NEURONS; i++)
+            counter #(
+                .CNT_WIDTH(CNT_WIDTH)
+            )
+            spike_counter(
+                .clk(clk),
+                .rst(rst),
+                .en(en && timestep_en),
+                
+                .signal(hidden_layer_spike[i]),
+                
+                .cnt(hidden_layer_spike_cnt[i])
+            );
+    endgenerate
+    
+    logic [3:0] assignment[HIDDEN_LAYER_NEURONS-1 : 0];  // class label assigned to each hidden layer neuron
+    logic [4 * HIDDEN_LAYER_NEURONS - 1 : 0] assignment_mem_out;
+//    neuron2class_assignment_mem u_assignment_mem(
+//        .clka(clk),
+//        .ena(en && timestep < TIMESTEP_MAX + 'd1),
+//        .addra(1'b0),
+//        .douta(assignment_mem_out)
+//    );
+    always_comb
+    begin
+        for (integer k = 0; k < HIDDEN_LAYER_NEURONS; k++)
+            assignment[k] = assignment_mem_out[k * 4 +: 4];
+    end
+    
+    // determine winner neuron (neuron with max spiking cnt)
+    //  and its assigned class is the final result
+    logic [6:0] winner_neuron;
+    max_comparer #(
+        .ITEM_CNT(HIDDEN_LAYER_NEURONS),
+        .DATA_WIDTH(CNT_WIDTH),
+        .TAG_WIDTH('d7)  // = width of HIDDEN_LAYER_NEURONS
+    ) u_spike_cnt_max(
+        .in_data(hidden_layer_spike_cnt),
+        .out_tag(winner_neuron)
+    );
+    assign result = assignment[winner_neuron];
+    
+endmodule
